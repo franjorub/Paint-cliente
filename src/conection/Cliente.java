228852d098e3fcea26aec93c0edef56132c918b5
Francisco Rubial <franjorub@gmail.com>
2018-05-06 16:46:22 -0430

    Creada interfaz funcional inicial del proyecto

---
+++ src/conection/Cliente.java
@@ -0,0 +1,37 @@
+package conection;
+
+import java.net.*;
+
+import java.io.*;
+
+/**
+ * @author Francisco Rubial
+ * @version 0.1.0
+ *          <h1>Conex</h1>
+ *          <p>
+ * 			Clase que crea la conexion como cliente y envia mensajes al servidor
+ *          por defecto se conecta en localhost y el puerto 5000
+ *          </p>
+ */
+public class Cliente {
+
+	final String HOST = "localhost";
+	final int PUERTO = 5000;
+	Socket sc;
+	DataOutputStream salida;
+
+	public Cliente(String mensaje) {
+
+		try {
+
+			sc = new Socket(HOST, PUERTO);
+			salida = new DataOutputStream(sc.getOutputStream());
+			salida.writeUTF(mensaje);
+			sc.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+	}
+
+}
